38ae81a3e29bc48046ea18738d3358dabd21077e
Gabriel-Nunes <gabriel_lima_nunes@hotmail.com>
2022-09-11 21:31:22 -0300

    first commit

---
+++ livecoin/spiders/coin_selenium.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.selector import Selector
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+
+class CoinSpiderSelenium(scrapy.Spider):
+    name = 'coin_selenium'
+    allowed_domains = ['www.livecoinwatch.com']
+    start_urls = ['https://www.livecoinwatch.com']
+    
+    def __init__(self):
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+
+        driver = webdriver.Chrome(executable_path='./chromedriver', options=chrome_options)
+        driver.set_window_size(1920, 1080)
+        driver.get("https://www.livecoinwatch.com")
+        
+        sleep(1)
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        sleep(4)
+        
+        self.html = driver.page_source
+        self.resp = Selector(text=self.html)
+        driver.close()
+
+    def parse(self, response):
+        for currency in self.resp.xpath("//tbody/tr"):
+            yield {
+                'Coin': currency.xpath(".//td[2]/a/div/div[contains(@class, 'item-name')]/small/text()").get(),
+                'Volume(24h)': currency.xpath(".//td[5]/text()").get()
+            }
